de111533fb12b1c76e6c9426723b9633cc36d887
Rajesh Sahu <ishurwara654@gmail.com>
2026-03-21 15:01:13 +0530

    Class Setup

--- CleartripAutomation/src/main/java/utility/ConfigReader.java
+++ CleartripAutomation/src/main/java/utility/ConfigReader.java
@@ -1,36 +1,29 @@
 package utility;
 
-
-package utils;
-
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
 
-    private static Properties prop;
+    public static Properties prop;
 
-    // Load the config file
     public static void loadConfig() {
-
-        prop = new Properties();
 
         try {
 
-            FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
+            prop = new Properties();
+
+            FileInputStream fis =
+                    new FileInputStream("C:\\Users\\rahul sahu\\git\\repository7\\CleartripAutomation\\src\\test\\resources\\config.properties");
 
             prop.load(fis);
 
-        } catch (IOException e) {
+        } catch (Exception e) {
 
-            System.out.println("Config file not found");
             e.printStackTrace();
-
         }
     }
 
-    // Get value from config file
     public static String getProperty(String key) {
 
         return prop.getProperty(key);
